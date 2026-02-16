ce460d17bf12215df5d2ca54c91b6348381b83a3
MeloneCodeUK <melone.ukraine.developer@hotmail.com>
2026-02-16 22:29:20 +0100

    commit

--- src/Screens/ProfileScreen/ProfileScreenPhone.jsx
+++ src/Screens/ProfileScreen/ProfileScreenPhone.jsx
@@ -186,7 +186,7 @@ export default function ProfileScreen({ navigation }) {
                   showAnimeDetails={profile.showAnimeDetails}
                   getAnimeFromItem={(item) => item.anime}
                   cardWidth={
-                    isTablet() ? SCREEN_WIDTH * 0.2 : SCREEN_WIDTH * 0.28
+                    isTablet() ? SCREEN_WIDTH * 0.2 : SCREEN_WIDTH * 0.35
                   }
                   numColumns={isTablet() ? 5 : 3}
                 />
